4be799bcb596205f94b37ae92a067a183ea1cd3a
Michal Dziedzic <2dziedzic.michal@gmail.com>
2022-08-25 20:33:20 +0200

    User class added

--- 4MD_console/src/main/java/com/infoshareacademy/Main.java
+++ 4MD_console/src/main/java/com/infoshareacademy/Main.java
@@ -2,6 +2,9 @@ package com.infoshareacademy;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        User user = new User("elo", "mordo");
+
     }
 }
