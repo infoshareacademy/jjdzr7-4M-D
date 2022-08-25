e8c38c8f8a37d79fd30965d0786453edc85c18ab
Mikołaj Chajewski <mikolajchajewski@gmail.com>
2022-08-25 10:42:55 +0200

    fixed debug code

--- 4MD_console/src/main/java/com/infoshareacademy/Main.java
+++ 4MD_console/src/main/java/com/infoshareacademy/Main.java
@@ -6,9 +6,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Recipe> recipes = new ArrayList<>();
-        recipes.add(new Recipe("test123",100));
-        recipes.add(new Recipe("kawa mrozona",500));
-        recipes.add(new Recipe("minecraft",660));
+        recipes.add(new Recipe(0,"minecraft",new ArrayList<>(),0,0,660,10,"Test food"));
+        recipes.add(new Recipe(1,"kawa",new ArrayList<>(),0,0,60,10,"Test food"));
+        recipes.add(new Recipe(2,"spagetti",new ArrayList<>(),0,0,1000,10,"Test food"));
+        recipes.add(new Recipe(3,"asdxczefr2efefefwwq",new ArrayList<>(),0,0,200,10,"Test food"));
         RecipePicker picker = new RecipePicker(recipes);
         Recipe recipe = picker.activate();
         System.out.println(recipe.getName());
