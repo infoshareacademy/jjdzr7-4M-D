11f566946d5ce46c45d263363f5a435c32391c80
Michał Woźniak <michal.wozniak86@int.pl>
2022-09-07 23:02:46 +0200

    add code edit choice menu

--- 4MD_console/src/main/java/com/infoshareacademy/EditorRecipes.java
+++ 4MD_console/src/main/java/com/infoshareacademy/EditorRecipes.java
@@ -9,8 +9,30 @@ public class EditorRecipes {
         this.recipe = recipe;
     }
 
+    public EditorRecipes() { recipe = getRecipe(); }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
     public void menu() {
-        System.out.println("Wybierz co edytować:\n1. Nazwę\n2. Ocenę\n3. Anuluj");
+        System.out.print("Wybierz co edytować:\n1. Nazwę\n2. Ocenę\n" +
+                "3. Anuluj\nTwój wybór to: ");
+    }
+
+    public void decisionMenu() {
+        if (choice() == 1) {
+            recipeId();
+            choice();
+        } else if (choice() == 2) {
+            recipeId();
+            choice();
+        } else if (choice() == 3) {
+            //!!!!!!!!!!!!!!!!!!!!!!
+            System.out.println("");
+        }
+        else
+            System.out.println("Powinieneś wybrać jeszcze raz.");
     }
 
     public void recipeId() {
