6456c4d7f332ca7bd71d39dfe46ecd1da3000c90
Mikołaj Chajewski <mikolajchajewski@gmail.com>
2022-09-16 23:09:36 +0200

    Completed Initial saver

--- 4MD_console/src/main/java/com/infoshareacademy/JsonRecipeManager.java
+++ 4MD_console/src/main/java/com/infoshareacademy/JsonRecipeManager.java
@@ -3,30 +3,43 @@ package com.infoshareacademy;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class JsonRecipeManager extends ArrayList<Recipe> {
-    private final Path PATH = Path.of("./recipes");
+public class JsonRecipeManager {
+    protected String RECIPES_PATH = "./recipes/";
 
     public JsonRecipeManager() throws IOException {
-        Files.createDirectory(PATH);
+        try{
+
+            Files.createDirectory(Path.of(RECIPES_PATH));
+        }
+        catch (FileAlreadyExistsException x){
+            // Great, who asked?
+        }
 
     }
-    public void save(Recipe recipe){
+    public void save(Recipe recipe) throws IOException {
         String value = JSONObject.valueToString(recipe);
+        Files.writeString(Path.of(RECIPES_PATH+recipe.getId()+".json"),value);
     }
-    private void loadJsonFile(String path){
-
+    public Recipe loadJsonFile(int recipeId) throws IOException {
+        String file = Files.readString(Path.of(RECIPES_PATH+recipeId+".json"));
+        return (Recipe) JSONObject.stringToValue(file);
     }
+    //Pls load this to cache
     public List<Recipe> loadAll() throws IOException {
-        Stream<Path> files = Files.list(PATH);
+        List<Recipe> out = new ArrayList<>();
+        Stream<Path> files = Files.list(Path.of(RECIPES_PATH));
         for (Object file : files.filter(path -> path.getFileName().endsWith(".json")).toArray()) {
             Path path = (Path) file;
-
+            String id = path.getFileName().toString().split("\\.")[0];
+            out.add(loadJsonFile(Integer.getInteger(id)));
         }
+        return out;
     }
 }
---
+++ 4MD_console/src/test/java/com/infoshareacademy/JsonTest.java
@@ -0,0 +1,47 @@
+package com.infoshareacademy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class JsonTest {
+    private final JsonTestSubject testedManager;
+    private final ArrayList<Recipe> recipes = new ArrayList<>();
+
+    public JsonTest() throws IOException {
+        testedManager = new JsonTestSubject();
+        recipes.add(new Recipe(0, "minecraft", new ArrayList<>(), 0, 0, 660, 10, "gra"));
+        recipes.add(new Recipe(1, "kawa", new ArrayList<>(), 0, 0, 60, 10, "jedzenie"));
+        recipes.add(new Recipe(2, "spagetti", new ArrayList<>(), 0, 0, 1000, 10, "Test_food"));
+        recipes.add(new Recipe(3, "asdxczefr2efefefwwq", new ArrayList<>(), 0, 0, 200, 10, "Test_food"));
+    }
+    @Test
+    public void TestWriteback() throws IOException {
+        for (Recipe recipe : recipes) {
+            testedManager.save(recipe);
+        }
+        List<Recipe> jsonRecipes = testedManager.loadAll();
+        for (Recipe recipe : recipes) {
+            Recipe jsonRecipe = (Recipe)
+                    jsonRecipes.stream().filter(recipe1 -> recipe1.getId()==recipe1.getId()).toArray()[0];
+            //
+            Assertions.assertEquals(recipe.getName(),jsonRecipe.getName());
+            Assertions.assertEquals(recipe.getTypeOfFood(),jsonRecipe.getTypeOfFood());
+            Assertions.assertEquals(recipe.getKcal(),jsonRecipe.getKcal());
+        }
+    }
+    class JsonTestSubject extends JsonRecipeManager{
+
+
+        public JsonTestSubject() throws IOException {
+            this.RECIPES_PATH = "/tmp/"+ new Random().nextInt()+"/";
+            Files.createDirectory(Path.of(RECIPES_PATH));
+        }
+    }
+}
