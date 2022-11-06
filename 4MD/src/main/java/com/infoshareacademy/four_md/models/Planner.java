870bdecc4ac980e100eda91a3ac40ed60aae654e
Michal Dziedzic <2dziedzic.michal@gmail.com>
2022-11-06 20:21:58 +0100

    Planner class added

---
+++ 4MD/src/main/java/com/infoshareacademy/four_md/models/Planner.java
@@ -0,0 +1,34 @@
+package com.infoshareacademy.four_md.models;
+
+import com.infoshareacademy.four_md.model.Recipe;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public class Planner {
+    private Map<LocalDate, Recipe> map;
+
+    public Planner(Map<LocalDate, Recipe> map) {
+        this.map = map;
+    }
+
+    public Map<LocalDate, Recipe> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<LocalDate, Recipe> map) {
+        this.map = map;
+    }
+
+    public void addTask(LocalDate localDate, Recipe recipe) {
+        this.map.put(localDate, recipe);
+    }
+
+    public void deleteTask(LocalDate date) {
+        map.remove(date);
+    }
+
+    public void deleteAllTask() {
+        map.clear();
+    }
+}
