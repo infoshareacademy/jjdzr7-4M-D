69a613519a7f3092e29246e431eba9270165c868
Michal Dziedzic <2dziedzic.michal@gmail.com>
2022-11-06 20:25:33 +0100

    clear unused imports

--- 4MD/src/main/java/com/infoshareacademy/four_md/Application.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/Application.java
@@ -1,15 +1,7 @@
 package com.infoshareacademy.four_md;
 
-import com.infoshareacademy.four_md.model.*;
-import com.infoshareacademy.four_md.models.Planner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 public class Application {
