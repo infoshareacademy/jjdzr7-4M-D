2e1ff0630fdbbb305520bf9a05c1f25f7d8b4e3d
Mikołaj Chajewski <mikolajchajewski@gmail.com>
2022-11-13 11:36:12 +0100

    fixed BookCookController

--- 4MD/src/main/java/com/infoshareacademy/four_md/controllers/BookCookController.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/controllers/BookCookController.java
@@ -1,18 +1,11 @@
 package com.infoshareacademy.four_md.controllers;
 
+import com.infoshareacademy.four_md.model.Recipe;
+import com.infoshareacademy.four_md.service.interfaces.RecipeProvider;
+import org.springframework.web.bind.annotation.*;
+
 import java.io.IOException;
 import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.infoshareacademy.four_md.models.Recipe;
-import com.infoshareacademy.four_md.service.interfaces.RecipeProvider;
 
 @RestController
 @RequestMapping("/BookCook")
@@ -24,11 +17,11 @@ public class BookCookController {
         
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public Recipe getRecipe(@PathVariable int id) throws IOException{
             return recipeProvider.get(id);
     }
-    @GetMapping("all")
+    @GetMapping("/all")
     public List<Recipe> getRecipes() throws IOException{
             return recipeProvider.getAll();
     }
@@ -36,7 +29,7 @@ public class BookCookController {
     public void addRecipe(@RequestBody Recipe recipe) throws IOException{
         recipeProvider.save(recipe);
     }
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public void deleteRecipe(@PathVariable int id) throws IOException{
         recipeProvider.remove(id);
     }
