d678ac1ddde1476441ee06b21c5051ac1c0ea218
DanielPopielski <danielek6699@wp.pl>
2022-11-20 14:40:14 +0100

    fixed User Class

--- 4MD/src/main/java/com/infoshareacademy/four_md/models/User.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/models/User.java
@@ -1,27 +1,25 @@
 package com.infoshareacademy.four_md.models;
 import java.util.List;
-import java.util.UUID;
 
 public class User {
-    private String id;
+    private int id;
     private String name;
     private String surname;
     private List<Recipe> listOfRecipes;
 
-    public User(String id, String name, String surname, List<Recipe> listOfRecipes) {
+    public User(int id, String name, String surname, List<Recipe> listOfRecipes) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.listOfRecipes = listOfRecipes;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId() {
-        UUID uuid = UUID.randomUUID();
-        this.id = uuid.toString();
+    public final void setId() {
+        id++;
     }
 
     public String getName() {
