0d00fcb5b782dc00872fec5b5c1e4e7ece262690
Michał Woźniak <michal.wozniak86@int.pl>
2022-12-11 18:25:44 +0100

    sall change

--- 4MD/src/main/java/com/infoshareacademy/four_md/model/SearchEngine.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/model/SearchEngine.java
@@ -1,10 +1,28 @@
 package com.infoshareacademy.four_md.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchEngine {
-    private List<Recipe> search;
+    private List<Recipe> name;
+    private List<Recipe> cost;
+    private List<Recipe> calories;
 
 
 
+    public List<Recipe> searchByTitle(String name) {
+        List<Recipe> arraylist = new ArrayList<Recipe>();
+
+        return arraylist;
+    }
+
+    public List<Recipe> searchByCost(double cost) {
+
+    }
+
+    public List<Recipe> searchByCalories(int calories) {
+        return
+    }
 }
