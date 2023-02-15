d3c094dd451cbcc489b8b796072cc09f48ec2f0e
Mikołaj Chajewski <mikolajchajewski.business@gmail.com>
2023-02-15 21:26:16 +0000

    fixed authentication

--- 4MD/src/main/java/com/infoshareacademy/four_md/config/WelcomeConfig.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/config/WelcomeConfig.java
@@ -1,26 +1,42 @@
 package com.infoshareacademy.four_md.config;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class WelcomeConfig extends WebSecurityConfigurerAdapter {
+public class WelcomeConfig{
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("mikolka").password("mikolka");
+    @Bean
+    public BCryptPasswordEncoder createEncoder(){
+        return new BCryptPasswordEncoder();
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder passwordEncoder) {
+        var userDb = new InMemoryUserDetailsManager();
+        userDb.createUser(
+                User.withUsername("mikolka")
+                .password(passwordEncoder.encode("mikolka"))
+                .roles("USER")
+                .build());
+        return userDb;
+    }
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/welcome")
-                .permitAll();
+                .permitAll()
+                .successForwardUrl("/BookCook")
+        ;
+        return http.build();
     }
 }
--- 4MD/src/main/java/com/infoshareacademy/four_md/controller/WelcomePageController.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/controller/WelcomePageController.java
@@ -1,6 +1,6 @@
 package com.infoshareacademy.four_md.controller;
 
-import org.springframework.http.RequestEntity;
+import com.infoshareacademy.four_md.model.UserCredentials;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomePageController {
 
     @GetMapping("/welcome")
-    public String welcome(RequestEntity info) {
+    public String welcome(Model model) {
+        model.addAttribute("credentials",new UserCredentials());
         return "welcome";
     }
 }
