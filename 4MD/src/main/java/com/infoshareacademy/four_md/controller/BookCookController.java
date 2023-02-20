22e047680cc660eceaf6575d06927730a52a2b2b
Michał Woźniak <michal.wozniak86@int.pl>
2023-02-20 11:43:20 +0100

    @GetMapping

--- 4MD/src/main/java/com/infoshareacademy/four_md/controller/BookCookController.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/controller/BookCookController.java
@@ -1,32 +1,31 @@
 package com.infoshareacademy.four_md.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.infoshareacademy.four_md.model.dto.Recipe;
+import com.infoshareacademy.four_md.model.entitiy.RecipeEntity;
+import com.infoshareacademy.four_md.service.jpaRepos.RecipeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/BookCook")
+@RequiredArgsConstructor
 public class BookCookController {
-//    private RecipeProvider recipeProvider;
-//
-//    public BookCookController(RecipeProvider recipeProvider) {
-//        this.recipeProvider = recipeProvider;
-//
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Recipe getRecipe(@PathVariable int id) throws IOException{
-//            return recipeProvider.get(id);
-//    }
-//    @GetMapping("/all")
-//    public List<Recipe> getRecipes() throws IOException{
-//            return recipeProvider.getAll();
-//    }
-//    @PostMapping()
-//    public void addRecipe(@RequestBody Recipe recipe) throws IOException{
-//        recipeProvider.save(recipe);
-//    }
-//    @DeleteMapping("/{id}")
-//    public void deleteRecipe(@PathVariable int id) throws IOException{
-//        recipeProvider.remove(id);
-//    }
+
+    private final RecipeRepository recipeRepository;
+
+    @GetMapping("/recipes")
+    public List<RecipeEntity> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
+
+    @GetMapping("/recipes/{id}")
+    public ResponseEntity<RecipeEntity> getRecipeById(@PathVariable int id) {
+        return ResponseEntity.of(recipeRepository.findById(id));
+    }
+
+
 }
