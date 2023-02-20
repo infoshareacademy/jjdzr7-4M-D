42e641d3cd336306361b878561b4ca08a848e77c
Michał Woźniak <michal.wozniak86@int.pl>
2023-02-20 11:46:46 +0100

    @PostMapping @PutMapping problems

--- 4MD/src/main/java/com/infoshareacademy/four_md/controller/BookCookController.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/controller/BookCookController.java
@@ -26,6 +26,38 @@ public class BookCookController {
     public ResponseEntity<RecipeEntity> getRecipeById(@PathVariable int id) {
         return ResponseEntity.of(recipeRepository.findById(id));
     }
+/*
+    @PostMapping(value = "/recipes")
+    public RecipeEntity createNewRecipe(@RequestBody Recipe recipe) {
+        Recipe newRecipe = new Recipe();
+        newRecipe.setName(recipe.getName());
+        newRecipe.setIngredientsList(recipe.getIngredientsList());
+        newRecipe.setEstimatedCookingTime(recipe.getEstimatedCookingTime());
+        newRecipe.setCalories(recipe.getCalories());
+        newRecipe.setRatingsList(recipe.getRatingsList());
+        newRecipe.setDifficulty(recipe.getDifficulty());
+        newRecipe.setDishType(recipe.getDishType());
+
+        return recipeRepository.save(newRecipe);
+    }
+
+    @PutMapping("/recipes/{id}")
+    public ResponseEntity<RecipeEntity> updateRecipe(@PathVariable int id, @RequestBody Recipe recipe) {
+        Optional<RecipeEntity> optionalRecipeEntity = recipeRepository.findById(id)
+                .map(recipeEntity -> {
+                    recipeEntity.setName(recipe.getName());
+                    recipeEntity.setIngredientsList(recipe.getIngredientsList());
+                    recipeEntity.setEstimatedCookingTime(recipe.getEstimatedCookingTime());
+                    recipeEntity.setCalories(recipe.getCalories());
+                    recipeEntity.setRatingsList(recipe.getRatingsList());
+                    recipeEntity.setDifficulty(recipe.getDifficulty());
+                    recipeEntity.setDishType(recipe.getDishType());
+                    return recipeEntity;
+                })
+                .map(recipeRepository::save);
+        return ResponseEntity.of(optionalRecipeEntity);
+    }
+*/
 
     @DeleteMapping("/recipes/{id}")
     public void deleteRecipe(@PathVariable int id) {
