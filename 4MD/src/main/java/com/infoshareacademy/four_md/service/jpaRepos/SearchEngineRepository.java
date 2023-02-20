54669441a440a1a15e13cb673159688558f39739
Michał Woźniak <michal.wozniak86@int.pl>
2023-02-20 11:40:06 +0100

    little change

--- 4MD/src/main/java/com/infoshareacademy/four_md/service/jpaRepos/SearchEngineRepository.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/service/jpaRepos/SearchEngineRepository.java
@@ -1,14 +1,13 @@
 package com.infoshareacademy.four_md.service.jpaRepos;
 
 import com.infoshareacademy.four_md.model.entitiy.RecipeEntity;
-import com.infoshareacademy.four_md.model.entitiy.SearchEngineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface SearchEngineRepository extends JpaRepository<RecipeEntity, Integer> {
 
-    List<RecipeEntity> findByName(String name);
+    List<RecipeEntity> findByNameContains(String name);
     List<RecipeEntity> findByCaloriesBetween(Integer calMin, Integer calMax);
     //List<RecipeEntity> findByPriceBetween(Double priceMin, Double priceMax);
 }
