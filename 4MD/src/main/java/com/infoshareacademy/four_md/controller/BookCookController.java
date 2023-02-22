4ef65c07330daa1c44020f5709ab04d3ef1ce14d
Michał Woźniak <michal.wozniak86@int.pl>
2023-02-22 11:00:47 +0100

    Changed @RequestMapping to @GetMapping

--- 4MD/src/main/java/com/infoshareacademy/four_md/controller/BookCookController.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/controller/BookCookController.java
@@ -3,6 +3,7 @@ package com.infoshareacademy.four_md.controller;
 import com.infoshareacademy.four_md.model.entitiy.RecipeEntity;
 import com.infoshareacademy.four_md.service.jpaRepos.SearchEngineRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,12 @@ public class BookCookController {
 
     private final SearchEngineRepository searchEngineRepository;
 
-    @RequestMapping("/recipes/search/{name}")
+    @GetMapping("/recipes/search/{name}")
     public List<RecipeEntity> findByNameContains(@PathVariable String name) {
         return searchEngineRepository.findByNameContains(name);
     }
 
-    @RequestMapping(value = "/recipes/search/calories/", params = {"calMin", "calMax"})
+    @GetMapping(value = "/recipes/search/calories/", params = {"calMin", "calMax"})
     public List<RecipeEntity> findByCaloriesBetween(@PathVariable int calMin, @PathVariable int calMax) {
         return searchEngineRepository.findByCaloriesBetween(calMin, calMax);
     }
