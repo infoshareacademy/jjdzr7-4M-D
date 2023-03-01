277317129b882e01a66ad2f534da915e4aa1f6f5
Uczen_05 <mikolajchajewski@gmail.com>
2023-03-01 14:40:53 +0100

    schoola

--- 4MD/src/main/java/com/infoshareacademy/four_md/controller/WelcomePageController.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/controller/WelcomePageController.java
@@ -1,13 +1,15 @@
 package com.infoshareacademy.four_md.controller;
 
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomePageController {
 
     @GetMapping("/welcome")
-    public String welcome() {
+    public String welcome(RequestEntity info) {
         return "welcome";
     }
 }
