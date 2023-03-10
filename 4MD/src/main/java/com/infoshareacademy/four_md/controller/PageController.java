b165e562f71628a8d8abf1277f2f3a69a5ee515e
Michał Woźniak <michal.wozniak86@int.pl>
2023-03-10 23:12:59 +0100

    change name from WelcomePageController add pages

---
+++ 4MD/src/main/java/com/infoshareacademy/four_md/controller/PageController.java
@@ -0,0 +1,37 @@
+package com.infoshareacademy.four_md.controller;
+
+import com.infoshareacademy.four_md.model.UserCredentials;
+import com.infoshareacademy.four_md.model.entitiy.UserEntity;
+import com.infoshareacademy.four_md.service.jpaRepos.UserRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Data
+@RequiredArgsConstructor
+public class PageController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/welcome")
+    public String welcome(Model model) {
+        model.addAttribute("credentials",new UserCredentials());
+        return "welcome";
+    }
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("user", new UserEntity());
+        return "register";
+    }
+
+    @PostMapping("/register_success")
+    public String processRegister(UserEntity user) {
+        userRepository.save(user);
+        return "register_success";
+    }
+}
