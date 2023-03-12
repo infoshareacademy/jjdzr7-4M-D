41457cdfda0a2b2c27a33206e2c44cdc47ac122b
Michał Woźniak <michal.wozniak86@int.pl>
2023-03-12 13:40:29 +0100

    changes in PageController

--- 4MD/src/main/java/com/infoshareacademy/four_md/controller/PageController.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/controller/PageController.java
@@ -1,21 +1,26 @@
 package com.infoshareacademy.four_md.controller;
 
 import com.infoshareacademy.four_md.model.UserCredentials;
-import com.infoshareacademy.four_md.model.entitiy.UserEntity;
-import com.infoshareacademy.four_md.service.jpaRepos.UserRepository;
+import com.infoshareacademy.four_md.model.dto.User;
+import com.infoshareacademy.four_md.service.providers.DbUserProvider;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 @Controller
 @Data
 @RequiredArgsConstructor
 public class PageController {
 
-    private final UserRepository userRepository;
+    private final DbUserProvider dbUserProvider;
 
     @GetMapping("/welcome")
     public String welcome(Model model) {
@@ -25,13 +30,17 @@ public class PageController {
 
     @GetMapping("/register")
     public String registerPage(Model model) {
-        model.addAttribute("user", new UserEntity());
+        model.addAttribute("user", new User());
         return "register";
     }
 
     @PostMapping("/register_success")
-    public String processRegister(UserEntity user) {
-        userRepository.save(user);
+    public String processRegister(@ModelAttribute @RequestBody User user) throws IOException {
+        System.out.println(user);
+        User user1 = new User(5,"Ddd","Ddd","Ddd","Ddd",new ArrayList<>());
+        System.out.println(user1);
+        dbUserProvider.save(user);
+        dbUserProvider.save(user1);
         return "register_success";
     }
 
