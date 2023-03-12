f3aa56b06864a8d8ac3b6d4d3a57155598de1364
Michał Woźniak <michal.wozniak86@int.pl>
2023-03-12 16:57:31 +0100

    Needed toString

--- 4MD/src/main/java/com/infoshareacademy/four_md/model/dto/User.java
+++ 4MD/src/main/java/com/infoshareacademy/four_md/model/dto/User.java
@@ -1,15 +1,11 @@
 package com.infoshareacademy.four_md.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User  {
